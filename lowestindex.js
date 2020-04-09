615c5a4cd78f7f6ee3fc6304920893126649b8d0
Valiana <valiana.josic@gmail.com>
2020-04-08 21:28:08 -0400

    coding challenges

--- lowestindex.js
+++ lowestindex.js
@@ -1,29 +1,27 @@
 function lowestIndex(array) {
-	
+
   var lowestIndex = 0;
   var j = 0;
-  
-  var position =[];
+  var position = [];
 
-    for (i = 0; i < array.length; i++) {
-        if (array[i] < array[lowestIndex]) {
-          
-          smallervalue = array[i];
-          lowestIndex = i;
-        
-        }}
+  for (i = 0; i < array.length; i++) {
+    if (array[i] < array[lowestIndex]) {
 
-    for (i = 0; i < array.length; i++)
-    {
-      if(smallervalue === array[i])
-      {
-        position[j] = i;
-        j++;
-      }
+      smallervalue = array[i];
+      lowestIndex = i;
+
     }
-    
-   return position;
-	
+  }
+
+  for (i = 0; i < array.length; i++) {
+    if (smallervalue === array[i]) {
+      position[j] = i;
+      j++;
+    }
+  }
+
+  return position;
+
 }
-	
+
 console.log(lowestIndex([-1, -7, 1, 5, -7, 0, 3, -7, 0])); 
