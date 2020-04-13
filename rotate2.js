53349d2b626ff7eac61cbc6acd5ebc96fba73daa
Valiana <valiana.josic@gmail.com>
2020-04-12 20:16:15 -0400

    coding challenges

--- rotate2.js
+++ rotate2.js
@@ -1,16 +1,17 @@
 function rotLeft(a, d) {
-    let len = 0,j=0
-    var b =[]
-    len = a.length
-    for (let i = d; i < len; i++){
-        b[j] = a[i]
+    var j = 0
+    var rotatedarr = []
+    for (i = d; i < a.length; i++) {
+
+        rotatedarr[j] = a[i]
         j++
     }
-    for (let i = 0; i < d; i++){
-        b[j] = a[i]
+    for (i = 0; i < d; i++) {
+
+        rotatedarr[j] = a[i]
         j++
     }
-    return b
+    return rotatedarr
 }
 
-console.log(rotLeft([1, 2, 3], 2));
+console.log(rotLeft([1, 2, 3, 4, 5, 6], 5));
