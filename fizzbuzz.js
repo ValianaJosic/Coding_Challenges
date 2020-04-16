44a1c82177ed269814abab3ee892474de8b8b38a
Valiana <valiana.josic@gmail.com>
2020-04-16 14:41:18 -0400

    coding challenges

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,16 @@
-
-function fizzBuzz(x) {
-    for (var i = 1; i <= x; i++) {
-      if ( i % 3 == 0 && i % 5 == 0 ) {
-        console.log('FizzBuzz');
-      }
-      else if ( i % 3 == 0 ) {
-        console.log('Fizz');
-      }
-      else if ( i % 5 == 0 ) {
-        console.log('Buzz');
-      }
-      else {
-        console.log(i);
+const fizzBuzz = num => {
+    for(let i = 1; i <= num; i++) {
+ 
+      if(i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz')
+      } 
+        else if(i % 3 === 0) {
+        console.log('fizz')
+      } 
+        else if(i % 5 === 0) {
+        console.log('buzz')
+      } else {
+        console.log(i)
       }
     }
   }
