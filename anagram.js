6e66dee5519ca959d704357a5ece8b22cf2114bc
Valiana <valiana.josic@gmail.com>
2020-04-16 15:34:35 -0400

    coding challenges

---
+++ anagram.js
@@ -0,0 +1,12 @@
+function isAnagram(first, second) {
+
+  var a = first.toLowerCase();
+  var b = second.toLowerCase();
+
+  a = a.split("").sort().join("");
+  b = b.split("").sort().join("");
+
+  return a === b;
+}
+
+  console.log(isAnagram("friend", "finder"))
