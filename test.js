71cb7a6673ee2a3d84b4d196982e56988d5bdc2c
Valiana <valiana.josic@gmail.com>
2020-08-05 19:24:19 -0400

    Coding Challenges

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
-var b = 1;
-function outer() {
-    var b = 2
-    function inner() {
-        var b; // b is undefined
-        b++; // b is NaN
-        b = 3; // b is 3
-        console.log(b); // output "3"
+function averageArray(arr){
+	
+    var sum = 0;
+    var n = arr.length;
+  
+    for ( var i=0; i< arr.length; i++){
+      sum += arr[i];
     }
-    inner();
-}
-outer();
+       return sum / n;
+  
+    };
+  
+    console.log(averageArray([1,3,9,15,9,0]))
