1f54a271613d8584f083b51536053f7c4ef77dc3
ValianaJosic <valiana.josic@gmail.com>
2020-08-23 17:54:21 -0400

    Coding Challenges

--- test.js
+++ test.js
@@ -1,11 +1,16 @@
-var arr = [1, 2, 3];
-rotateRight(arr);
-console.log(arr);
+var arr = [1,2,3];
+var arr2 = reverseArray(arr);
+console.log(arr2);
 
-function rotateRight(arr) {
+function reverseArray(arr){
 
-    var last = arr.pop();
-    arr.unshift(last);
+    var arr2 = [];
+
+    for(var i= arr.length - 1; i >= 0; i--){
+        arr2.push(arr[i]);
+    }
+    
+    return arr2;
 }
 
 
