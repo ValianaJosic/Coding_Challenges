216c8272bf295c7b7ac35e509308cb63b78aacfe
ValianaJosic <valiana.josic@gmail.com>
2020-08-30 17:52:50 -0400

    Coding Challenges

--- test.js
+++ test.js
@@ -1,32 +1,11 @@
-function lowestIndex(array) {
-
-    var lowestIndex = 0;
-    var j = 0;
-    var position = [];
+const evenOdd = n =>{
   
-    for (i = 0; i < array.length; i++) {
-  
-      if (array[i] <= array[lowestIndex]) {
-  
-        smallervalue = array[i];
-        lowestIndex = i;
-  
-      }
-    }
-  
-    for (i = 0; i < array.length; i++) {
-  
-      if (smallervalue === array[i]) {
-  
-        position[j] = i;
-        j++;
-        
-      }
-    }
-  
-    return position;
-  
+  if( n % 2 === 0){
+     console.log('EVEN')
+  }else{
+     console.log('ODD')
   }
-  
-  console.log(lowestIndex([-1, -7, 1, 5, -7, 0, 3, -7, 0])); 
+}
+
+evenOdd(2)
   
