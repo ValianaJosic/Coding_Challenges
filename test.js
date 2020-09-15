c0b562c6dcdbea16b2026b689a7e8d6bb7eb964e
ValianaJosic <valiana.josic@gmail.com>
2020-09-15 16:28:35 -0400

    Coding Challenges

--- test.js
+++ test.js
@@ -1,20 +1,14 @@
-function foo1()
-{
-  return {
-      bar: "hello"
-  };
+var s = reverseString("Javascript");
+console.log(s);
+
+function reverseString(s){
+
+    var s2 = "";
+
+    for( var i= s.length -1; i >= 0; i--){
+
+        var char= s[i];
+        s2 += char;
+    }
+    return s2;
 }
-
-function foo2()
-{
-  return
-  {
-      bar: "hello"
-  };
-}
-
-console.log("foo1 returns:");
-console.log(foo1());
-console.log("foo2 returns:");
-console.log(foo2());
-
