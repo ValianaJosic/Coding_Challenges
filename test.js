20d8cee14f22af10c487c69d14b0e95cd487d450
ValianaJosic <valiana.josic@gmail.com>
2020-09-20 17:01:33 -0400

    Coding Challenges

--- test.js
+++ test.js
@@ -1,17 +1,12 @@
-function rotLeft(a, d) {
-    var j = 0
-    var rotatedarr = []
-    for (i = d; i < a.length; i++) {
+function isAnagram(first, second) {
 
-        rotatedarr[j] = a[i]
-        j++
-    }
-    for (i = 0; i < d; i++) {
-
-        rotatedarr[j] = a[i]
-        j++
-    }
-    return rotatedarr
-}
-
-console.log(rotLeft([1, 2, 3, 4, 5, 6], 5));
+    var a = first.toLowerCase();
+    var b = second.toLowerCase();
+  
+    a = a.split("").sort().join("");
+    b = b.split("").sort().join("");
+  
+    return a === b;
+  }
+  
+    console.log(isAnagram("friend", "finder"))
