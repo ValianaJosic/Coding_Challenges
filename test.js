db6c2e3a94392f0ffc57a61e50c8672b4621d935
ValianaJosic <valiana.josic@gmail.com>
2020-09-21 19:53:22 -0400

    Coding Challenges

--- test.js
+++ test.js
@@ -1,15 +1,70 @@
-const findVowels = str => {
-
-    let count = 0
-    const vowels = ['a', 'e', 'i', 'o', 'u']
-
-    for(let char of str.toLowerCase()) {
-
-      if(vowels.includes(char)) {
-        count++
-      }
-    }
-    return count
+{
+  "Structure": {
+     "type": "Document",
+     "documentType": "document",
+     "componentProperties": {},
+     "themeData": {},
+     "children": [
+        {
+           "type": "Container",
+           "styleId": "style-ish4oam6",
+           "id": "SITE_HEADER",
+           "components": [
+              {
+                 "type": "Component",
+                 "styleId": "style-jliasy0h",
+                 "id": "comp-ish43ju9",
+                 "dataQuery": "#dataItem-j9nxzwid",
+                 "skin": "wysiwyg.common.components.dropdownmenu.viewer.skins.SolidColorMenuButtonSkin",
+                 "layout": {
+                    "width": 396,
+                    "height": 50,
+                    "x": 511,
+                    "y": 39,
+                    "scale": 1,
+                    "rotationInDegrees": 0,
+                    "fixedPosition": false
+                 },
+                 "propertyQuery": "propItem-ish43jvj",
+                 "componentType": "wysiwyg.viewer.components.menus.DropDownMenu"
+              },
+              {
+                 "type": "Component",
+                 "styleId": "style-j1xatrdl",
+                 "id": "comp-it16i3ly",
+                 "dataQuery": "#dataItem-it16i3m5",
+                 "skin": "wysiwyg.viewer.skins.TPAWidgetSkin",
+                 "layout": {
+                    "width": 28,
+                    "height": 34,
+                    "x": 921,
+                    "y": 44,
+                    "scale": 1,
+                    "rotationInDegrees": 0,
+                    "fixedPosition": false
+                 },
+                 "componentType": "wysiwyg.viewer.components.tpapps.TPAWidget"
+              },
+              {
+                 "type": "Component",
+                 "styleId": "txtNew",
+                 "id": "comp-isypcsil",
+                 "dataQuery": "#dataItem-isypcsj9",
+                 "layout": {
+                    "width": 336,
+                    "height": 55,
+                    "x": 15,
+                    "y": 39,
+                    "scale": 1,
+                    "rotationInDegrees": 0,
+                    "fixedPosition": false
+                 },
+                 "propertyQuery": "propItem-isypcsj91",
+                 "componentType": "wysiwyg.viewer.components.WRichText"
+              }
+           ]
+        }
+     ]
   }
+}
 
-  console.log(findVowels('Hola Senorita'))
