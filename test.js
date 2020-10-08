1961365cb6f3de3cbe208ad7a6c2922b287f2732
ValianaJosic <valiana.josic@gmail.com>
2020-10-07 21:28:52 -0400

    email sent confirmation

--- test.js
+++ test.js
@@ -1,23 +1,6 @@
-function minimumBribes(q) {
-  let bribeCount = []
-  let high = 0;
-  
-  for (let i = 0; i < q.length; i++) {
-      let val = q[i]
-      bribeCount[val] = 0
-      high = Math.max(val, high)  // update the highest value encountered
-    
-      if (val < high) {
-          // if current value < high value, increment value for all bribeCount indices > val
-          for (let j=val+1; j < bribeCount.length; j++) {
-              bribeCount[j]++
-              if (bribeCount[j] > 2) {
-                  console.log("Too chaotic")
-                  return;
-              }
-          }
-      }
-  }
-  const sum = bribeCount.reduce((a,b) => a + b, 0)  // sum
-  console.log(sum);
-}
+function filterPositives(arr){
+
+    return  arr.filter( el => el >= 0);
+ }
+ 
+ console.log(filterPositives([-5 , 10, -3 , 12, -9, 5, 90, 0, 1]));
