047805eef628a2e0e6884614d21319a1f22f53ab
ValianaJosic <valiana.josic@gmail.com>
2020-10-10 17:02:04 -0400

    Coding Challenges

--- test.js
+++ test.js
@@ -1,6 +1,23 @@
-(function() {
-    console.log(1); 
-    setTimeout(function(){console.log(2)}, 1000); 
-    setTimeout(function(){console.log(3)}, 0); 
-    console.log(4);
-})();
+function minimumBribes(q) {
+    let bribeCount = []
+    let high = 0;
+    
+    for (let i = 0; i < q.length; i++) {
+        let val = q[i]
+        bribeCount[val] = 0
+        high = Math.max(val, high)  // update the highest value encountered
+      
+        if (val < high) {
+            // if current value < high value, increment value for all bribeCount indices > val
+            for (let j=val+1; j < bribeCount.length; j++) {
+                bribeCount[j]++
+                if (bribeCount[j] > 2) {
+                    console.log("Too chaotic")
+                    return;
+                }
+            }
+        }
+    }
+    const sum = bribeCount.reduce((a,b) => a + b, 0)  // sum
+    console.log(sum);
+}
