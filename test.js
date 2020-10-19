fc0ab4d37db0afff4dddc441746910e3a60f3775
ValianaJosic <valiana.josic@gmail.com>
2020-10-19 19:16:25 -0400

    Coding Challenges

--- test.js
+++ test.js
@@ -1,14 +1,23 @@
-var s = reverseString("Javascript");
-console.log(s);
+//ES 5 Syntax Possible Solution:
 
-function reverseString(s){
-
-    var s2 = "";
-
-    for( var i= s.length -1; i >= 0; i--){
-
-        var char= s[i];
-        s2 += char;
-    }
-    return s2;
+function longestString1(arr) {
+	var longest = ''; // Step 0
+	for (var i = 0; i < arr.length; i++) { // Step 1
+		if (arr[i].length > longest.length) { // Step 2 & 3
+			longest = arr[i]; // Step 4
+		}
+	}
+	return longest; // Step 5
 }
+
+//ES 6 Syntax Possible Solution:
+
+const longestString2 = (arr) => {
+	let longest = ''; // Step 0
+	arr.forEach((item) => { // Step 1
+		if(item.length > longest.length) {
+            longest = item;
+        }
+	});
+	return longest; // Step 5
+};
