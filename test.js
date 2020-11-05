e8bc42abb3e48ad33823068f6f965fe79add91f4
ValianaJosic <valiana.josic@gmail.com>
2020-11-05 17:06:39 -0500

    Coding Challenges

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-var arr = [1, 2, 3];
-rotateLeft(arr);
-console.log(arr);
-
-function rotateLeft(arr){
-
-    var first = arr.shift();
-    arr.push(first);
-
+var sum = 0;
+for (var x = 0; x < 1000; x++)
+{
+    if (x % 3 === 0 || x % 5 === 0)
+    {
+       sum += x;
+    }
 }
+console.log(sum);
