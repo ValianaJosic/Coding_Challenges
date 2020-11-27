cdf9d82a3c516945333f5a2997e5d437a1178d2c
ValianaJosic <valiana.josic@gmail.com>
2020-11-27 17:02:28 -0500

    Coding Challenges

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-function sum(a, b, c) {
-    return a + b + c;
-  }
-  
-  function sum(a, b) {
-    return a + b;
-  }
-  
-  var result = sum(5, 10, 10);
-  console.log(result);
+var sum = 0;
+for (var x = 0; x < 1000; x++)
+{
+    if (x % 3 === 0 || x % 5 === 0)
+    {
+       sum += x;
+    }
+}
+console.log(sum);
