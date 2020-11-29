cd738e3bec46c3f017ca0972d7df653bf83b19f3
ValianaJosic <valiana.josic@gmail.com>
2020-11-29 17:02:34 -0500

    Coding Challenges

--- test.js
+++ test.js
@@ -1,66 +1,12 @@
-var input = require('readline-sync');
+function arraySum(arr){
 
-let name = []
+  var sum = 0;
 
-let answers = ['Sally Ride', 'True', '40', 'Trajectory', '3']
+  for ( var i=0; i < arr.length; i++){
+      sum += arr[i];
+  }
 
-let questions = ['1) Who was the first American woman in space?',
-    '2) True or false: 5000 meters = 5 kilometers.',
-    '3) (5 + 3)/2 * 10 = ?',
-    '4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?',
-    '5) What is the minimum crew size for the International Space Station (ISS)?']
-
-let count = 0
-let grade = []
-
-const App = () => {
-
-    let response = []
-
-    let cand = input.question('Candidate Name: ')
-
-    name.push(cand)
-
-    for (i = 1; i < questions.length; i++) {
-
-        for (i = 0; i < answers.length; i++) {
-
-            let resp1 = input.question(questions[i])
-            response.push(resp1)
-            let ans = `Correct Answer: ${answers[i]}`
-            let res = `Your Answer:  ${response[i]}`
-            console.log(ans, '\n', res)
-        }
-
-        for (i = 0; i < response.length; i++) {
-            for (i = 0; i < answers.length; i++) {
-
-                let answerValue = answers[i].toLowerCase()
-                let responseValue = response[i].toLowerCase()
-
-                if (responseValue === answerValue) {
-
-                    count++
-
-                }
-            }
-        }
-
-        let grad = (count) / (5) * 100
-        grade.push(grad)
-
-        console.log('>>> Overall Grade: ' + grade + '% <<<')
-
-    }
-
-    if (grade < 60) {
-
-        console.log('>>> Status: Failed <<<')
-    }
-    else {
-        console.log('>>> Status: Passed <<<')
-    }
-
-
+  return sum;
 }
-App()
+
+console.log(arraySum([2, 3, -1, 5, 7, 9, 10, 15, 95]))
