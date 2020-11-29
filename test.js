9befee70b10f085313c0d20745bf0c37baba65ba
ValianaJosic <valiana.josic@gmail.com>
2020-11-29 17:01:51 -0500

    Coding Challenges

--- test.js
+++ test.js
@@ -1,31 +1,66 @@
-function lowestIndex(array) {
+var input = require('readline-sync');
 
-  var lowestIndex = 0;
-  var j = 0;
-  var position = [];
+let name = []
 
-  for (i = 0; i < array.length; i++) {
+let answers = ['Sally Ride', 'True', '40', 'Trajectory', '3']
 
-    if (array[i] <= array[lowestIndex]) {
+let questions = ['1) Who was the first American woman in space?',
+    '2) True or false: 5000 meters = 5 kilometers.',
+    '3) (5 + 3)/2 * 10 = ?',
+    '4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?',
+    '5) What is the minimum crew size for the International Space Station (ISS)?']
 
-      smallervalue = array[i];
-      lowestIndex = i;
+let count = 0
+let grade = []
+
+const App = () => {
+
+    let response = []
+
+    let cand = input.question('Candidate Name: ')
+
+    name.push(cand)
+
+    for (i = 1; i < questions.length; i++) {
+
+        for (i = 0; i < answers.length; i++) {
+
+            let resp1 = input.question(questions[i])
+            response.push(resp1)
+            let ans = `Correct Answer: ${answers[i]}`
+            let res = `Your Answer:  ${response[i]}`
+            console.log(ans, '\n', res)
+        }
+
+        for (i = 0; i < response.length; i++) {
+            for (i = 0; i < answers.length; i++) {
+
+                let answerValue = answers[i].toLowerCase()
+                let responseValue = response[i].toLowerCase()
+
+                if (responseValue === answerValue) {
+
+                    count++
+
+                }
+            }
+        }
+
+        let grad = (count) / (5) * 100
+        grade.push(grad)
+
+        console.log('>>> Overall Grade: ' + grade + '% <<<')
 
     }
-  }
 
-  for (i = 0; i < array.length; i++) {
+    if (grade < 60) {
 
-    if (smallervalue === array[i]) {
-
-      position[j] = i;
-      j++;
-      
+        console.log('>>> Status: Failed <<<')
     }
-  }
+    else {
+        console.log('>>> Status: Passed <<<')
+    }
 
-  return position;
 
 }
-
-console.log(lowestIndex([-1, -7, 1, 5, -7, 0, 3, -7, 0])); 
+App()
