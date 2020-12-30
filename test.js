73e6e8e375d5c86089dee631ccb572b0d5a39a74
ValianaJosic <valiana.josic@gmail.com>
2020-12-30 17:03:46 -0500

    Coding Challenges

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
-function sum(a, b, c) {
-    return a + b + c;
-  }
-  
-  function sum(a, b) {
-    return a + b;
-  }
-  
-  var result = sum(5, 10, 10);
-  console.log(result);
+let arr = ["1", "Test User", "25", "Developer"];
+let arr1 = [["id", "1"],["name", "Test User"],["age", "25"],["profession", "Developer"]];
+
+
+//Method 1: Using Object.assign() method
+console.log(Object.assign({}, arr));
+//{0: "1", 1: "Test User", 2: "25", 3: "Developer"}
+
+// Method 2 Using Spread operator
+console.log({ ...arr });
+//{0: "1", 1: "Test User", 2: "25", 3: "Developer"}
+
+// Method 3: Using Object.fromEntries() method
+console.log(Object.fromEntries(arr1));
+//{id: "1", name: "Test User", age: "25", profession: "Developer"}
+
   
