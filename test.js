bc6a6c543998592c782886fd28a6148030875992
ValianaJosic <valiana.josic@gmail.com>
2021-01-01 17:26:36 -0500

    Coding Challenges

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
-function greet (person) {
-  if (person !== { name: 'amy' }) {
-    return 'hey amy'
-  } else {
-    return 'hey arnold'
+const fizzBuzz = num => {
+  for(let i = 1; i <= num; i++) {
+
+    if(i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz')
+    } 
+      else if(i % 3 === 0) {
+      console.log('fizz')
+    } 
+      else if(i % 5 === 0) {
+      console.log('buzz')
+    } else {
+      console.log(i)
+    }
   }
 }
 
-console.log(greet({ name: 'amy' }))
- 
+fizzBuzz(20);
