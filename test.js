669f10c274e7c1114e9b503c46618d31ddad3ffb
ValianaJosic <valiana.josic@gmail.com>
2021-02-20 17:02:13 -0500

    Coding Challenges

--- test.js
+++ test.js
@@ -1,5 +1,34 @@
-const sortObjects = (arr) => {
-    return arr.sort((a, b) => (a.population > b.population) ? 1 : 1); // -1 decreasing order
-  }
+function printPrimes(nPrimes){
+  var n = 0;
+  var i = 2;
 
-console.log(sortObjects([1, 11, 27, 2, 34, 123])); 
+  while(n < nPrimes){
+
+      if(isPrime(i)){
+          console.log(n, " --> ", i);
+          n++
+      }
+      i++;
+  }
+}
+
+function isPrime(n){
+if(n < 2)
+  return false;
+
+if(n == 2) 
+return true;
+
+var maxDiv = Math.sqrt(n);
+
+for(var i=2; i <= maxDiv; i++){
+
+  if ( n % i == 0){
+      return false;
+  }
+}
+return true;
+
+}
+
+console.log(printPrimes(100))
