abc1eb4ea8378ce53b7e690d40fa76f388b91e37
ValianaJosic <valiana.josic@gmail.com>
2021-02-25 17:59:37 -0500

    Coding Challenges

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
-const anagrams = (str1, str2) => {
-  if(str1.length !== str2.length) { //step 1
-      return false;
-  }
-  let sort1 = str1.split('').sort(); // step 2
-  let sort2 = str2.split('').sort();
-  return sort1.join('') === sort2.join('');
+function celsiusToFahrenheit(n)
+{
+    return n * 1.8 + 32;
 }
+
+var r = celsiusToFahrenheit(20);
+println(r);
